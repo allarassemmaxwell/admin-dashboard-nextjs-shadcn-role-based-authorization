ed3fc3263a4a4a1befcb4d544edadc040ebb5d28
Allarassem Maxime <allarassemmaxime@Allarassems-MacBook-Air.local>
2025-07-26 03:38:09 +0300

    Implementing the charts

--- src/app/admin/actions.ts
+++ src/app/admin/actions.ts
@@ -7,3 +7,22 @@ export async function getSubscriptionsCount() {
     const data = await db.select({count: count()}).from(subscriptionsTable);
     return data[0].count;
 }
+
+
+export async function getSubscriptionBreakdown() {
+    const breakdown = await db.select({
+        plan: subscriptionsTable.plan,
+        total: sql<number>`count(*)`,
+    }).from(subscriptionsTable)
+    .where(and(
+        or(
+            eq(subscriptionsTable.plan, "basic"),
+            eq(subscriptionsTable.plan, "premium"),
+            eq(subscriptionsTable.plan, "free"),
+        ),
+        isNull(subscriptionsTable.endDate)
+    ))
+    .groupBy(subscriptionsTable.plan)
+
+    return breakdown;
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,8 @@ import { UsersTable, type User } from "@/components/users-table";
 import { Users, DollarSign, CreditCard, Activity } from "lucide-react";
 import { AdBanner } from "@/components/ad-banner";
 import { QuickLinks } from "@/components/quick-links";
-import { getSubscriptionsCount } from "@/app/admin/actions";
-import { log } from "console";
+import { getSubscriptionBreakdown, getSubscriptionsCount } from "@/app/admin/actions";
+import { ChartPie } from "@/components/chart-pie";
 
 
 const users: User[] = [
@@ -41,8 +41,7 @@ const users: User[] = [
 
 export default async function Home() {
     const subscriptions = await getSubscriptionsCount();
-    console.log("==== subscriptions ====");
-    console.log(subscriptions);
+    const subsBreakdown = await getSubscriptionBreakdown();
     
     const metrics: Metrice[] = [
         {
@@ -77,12 +76,23 @@ export default async function Home() {
         <main className="container mx-auto p-4 space-y-4">
             <h1 className="text-3xl font-bold mb-6">Dashboard</h1>
             <MetricCards metrics={metrics} />
-            <div className="flex flex-row items-center md:flex-col space-y-4 space-x-4">
-                <AdBanner />
-                <QuickLinks />
+            <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
+                {/* <div className="md:col-span-2">
+                    <ChartLine data={subsPerMonth} />
+                </div> */}
+                <div className="flex flex-row space-y-4">
+                    <AdBanner />
+                    <QuickLinks />
+                </div>
             </div>
-            <div>
-                <UsersTable data={users} />
+            <div className="grid grid-cols-1 lg:grid-cols-3 gap-4">
+                <div className="lg:col-span-2">
+                    <div className="bg-card rounded-lg p-6 shadow-sm">
+                        <h2 className="text-xl font-bold mb-4">Recent Users</h2>
+                        <UsersTable data={users} />
+                    </div>
+                </div>
+                <ChartPie data={subsBreakdown} />
             </div>
         </main>
     );
